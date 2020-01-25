500b340da778ea3f17d7aee4c0f3ef4ba5e5be47
awberez <alex@berez.com>
2020-01-24 20:56:30 -0600

    added stuffalexwants.com

--- src/components/GalleryTile/GalleryTile.js
+++ src/components/GalleryTile/GalleryTile.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./GalleryTile.css";
 
 const GalleryTile = props => (          
-	<div className="col-4 col-sm-2 col-lg-6">
+	<div className="col-4 col-sm-3 col-lg-6">
   		<div className="tile" onClick={() => props.selectTile(props.id)}>
   			<img src={props.image} className="tileImage" alt={props.name}></img>
   		</div>
